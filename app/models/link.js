0b480c0c66ae372af21f0b2397b92917f5d80759
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 17:15:04 -0700

    refactored sign-up to work with MongoDB

--- app/models/link.js
+++ app/models/link.js
@@ -3,21 +3,4 @@ var crypto = require('crypto');
 
 var Link = db.model('link', db.urlsSchema);
 
-
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
-// module.exports = Link;
+module.exports = Link;
